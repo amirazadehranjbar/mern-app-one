c6b6fcda442ce8eb31b2393af1b06704cf7b8bb2
Amir Azadeh Ranjbar <78546538+amirazadehranjbar@users.noreply.github.com>
2026-08-08 07:30:43 -0700

    frontend : add NavBar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,12 +1,56 @@
 function NavBar() {
     return (
-        <div className="sticky top-0 w-full h-12">
-            <select data-choose-theme className="select">
-                <option value="light">Light</option>
-                <option value="dark">Dark</option>
-                <option value="dracula">Dracula</option>
-                <option value="forest">Forest</option>
-            </select>
+        <div className="navbar bg-base-100 shadow-sm">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                             stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                  d="M4 6h16M4 12h8m-8 6h16"/>
+                        </svg>
+                    </div>
+                    <ul
+                        tabIndex="-1"
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        <li><a>Item 1</a></li>
+                        <li>
+                            <a>Parent</a>
+                            <ul className="p-2">
+                                <li><a>Submenu 1</a></li>
+                                <li><a>Submenu 2</a></li>
+                            </ul>
+                        </li>
+                        <li><a>Item 3</a></li>
+                    </ul>
+                </div>
+                <a className="btn btn-ghost text-xl">daisyUI</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li><a>Item 1</a></li>
+                    <li>
+                        <details>
+                            <summary>Parent</summary>
+                            <ul className="p-2 bg-base-100 w-40 z-1">
+                                <li><a>Submenu 1</a></li>
+                                <li><a>Submenu 2</a></li>
+                            </ul>
+                        </details>
+                    </li>
+                    <li><a>Item 3</a></li>
+                </ul>
+            </div>
+            <div className="navbar-end">
+
+                    <select data-choose-theme className="select w-1/5">
+                        <option value="light">Light</option>
+                        <option value="dark">Dark</option>
+                        <option value="dracula">Dracula</option>
+                        <option value="forest">Forest</option>
+                    </select>
+
+            </div>
         </div>
     )
 }
